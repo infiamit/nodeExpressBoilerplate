60012fbbc0b6d91ea44a980756128504f69bfa4f
infiamit <amitsingh80108@gmail.com>
2019-09-01 00:26:28 +0530

    production fixes

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require('compression');
+
 
 var indexRouter = require('./controllers/index');
 
 var app = express();
-
+if (process.env.NODE_ENV === 'production') {
+    app.disable('x-powered-by');
+    app.use(compression())
+}
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,5 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+
 
 module.exports = app;
